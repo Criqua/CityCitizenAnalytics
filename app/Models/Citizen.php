af86a25d95cf2db7e41d5e49d84a96598c542f6f
Cristian <cj83774932@gmail.com>
2025-05-24 20:03:14 -0600

    Add City and Citizen models along with their respective migrations.

---
+++ app/Models/Citizen.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Citizen extends Model
+{
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'birth_date',
+        'city_id',
+        'address',
+        'phone_number'
+    ];
+
+    public function city(){
+        return $this->belongsTo(City::class);
+    }
+
+    public function getFullName(){
+        return $this->first_name . ' ' . $this->last_name;
+    }
+
+    public function getAge(){
+        return \Carbon\Carbon::parse($this->birth_date)->age;
+    }
+
+    public function getCity(){
+        return $this->city->name;
+    }
+}
